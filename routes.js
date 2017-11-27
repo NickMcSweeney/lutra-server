733df566879c72b15a6e1c29e5029c603e7cd4b7
NickMcSweeney <shindler25@gmail.com>
2017-11-27 01:22:18 -0800

    admin login + verify routes

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 const assert = require("assert");
 const Mongo = require("mongodb");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+
 const Mongodb = Mongo.MongoClient;
 
 module.exports = {
@@ -23,7 +26,7 @@ module.exports = {
       ctx.response.status = 200;
       ctx.response.type = "application/json";
       ctx.response.body = {
-        text: `You have added ${header} to the Database`
+        text: `You have added ${header} to the Database`,
       };
       console.log("Finished input test", ctx.response);
       return ctx.response;
@@ -87,7 +90,7 @@ module.exports = {
       ctx.response.status = 200;
       ctx.response.type = "application/json";
       ctx.response.body = {
-        text: `You have added ${itemName} to the Database as ${user}`
+        text: `You have added ${itemName} to the Database as ${user}`,
       };
       console.log("Finished input test", ctx.response);
       return ctx.response;
@@ -110,5 +113,110 @@ module.exports = {
       console.log("Finished read test", ctx.response);
       return ctx.response;
     }
-  }
+  },
+  login: async ctx => {
+    console.log("Starting Login...");
+    try {
+      const url = "mongodb://localhost:27017/lutra";
+      const db = await Mongodb.connect(url);
+      const adminDB = db.collection("admin");
+      const data = await adminDB.find({}).toArray();
+      let username = "";
+      let password = "";
+      data.forEach(obj => {
+        if (obj.username) {
+          username = obj.username;
+        } else if (obj.password) {
+          password = obj.password;
+        }
+      });
+      console.log(
+        username + " vs " + ctx.request.body.username,
+        password + " vs " + ctx.request.body.password
+      );
+      if (
+        username === ctx.request.body.username &&
+        password === ctx.request.body.password
+      ) {
+        const cert = await fs.readFileSync("./cert/private/key.pem");
+        let auth = {};
+        auth[username] = password;
+
+        const token = await jwt.sign(auth, cert);
+
+        let body = new Object();
+        body.sucess = true;
+        body.jwt = token;
+        body.auth = auth;
+        body = JSON.stringify(body);
+
+        ctx.response.body = body;
+        ctx.response.status = 200;
+        ctx.response.type = "application/json";
+
+        console.log(
+          "Sucess ... Login Complete ... Return Values: ",
+          ctx.response
+        );
+
+        return ctx.response;
+      } else {
+        throw "falure - no match for account in db";
+      }
+    } catch (e) {
+      console.error(e);
+
+      let body = new Object();
+      body.sucess = false;
+      body = JSON.stringify(body);
+
+      ctx.response.type = "application/json";
+      ctx.response.body = body;
+      ctx.response.status = 400;
+
+      return ctx.response;
+    }
+  },
+  verifyToken: async ctx => {
+    const token = ctx.request.body.token;
+    const cert = fs.readFileSync("./cert/private/key.pem");
+    let decoded = null;
+    let auth = null;
+    try {
+      decoded = await jwt.verify(token, cert);
+      const url = "mongodb://localhost:27017/lutra";
+      const db = await Mongodb.connect(url);
+      const adminDB = db.collection("admin");
+      const data = await adminDB.find({}).toArray();
+      let username = "";
+      let password = "";
+      data.forEach(obj => {
+        if (obj.username) {
+          username = obj.username;
+        } else if (obj.password) {
+          password = obj.password;
+        }
+      });
+
+      if (decoded[username] === password) {
+        auth = true;
+      } else {
+        auth = false;
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      if (auth) {
+        ctx.response.body = { sucess: auth };
+        ctx.response.status = 200;
+        ctx.response.type = "application/json";
+        return ctx.response;
+      } else {
+        ctx.response.type = "application/json";
+        ctx.response.body = { sucess: false };
+        ctx.response.status = 400;
+        return ctx.response;
+      }
+    }
+  },
 };
--- server.js
+++ server.js
@@ -15,10 +15,12 @@ const myRoutes = require("./routes.js");
 
 route.get("/test/", myRoutes.test);
 route.get("/readAll/", myRoutes.readFromDb);
+route.get("/getNamedData/:item", myRoutes.getNamedData);
 route.post("/createUser/", myRoutes.createUser);
 route.post("/writeDbEncrypt/", myRoutes.writeDbEncrypt);
 route.post("/writeDb/", myRoutes.addToDb);
-route.get("/getNamedData/:item", myRoutes.getNamedData);
+route.post("/login/", myRoutes.login);
+route.post("/verifyToken/", myRoutes.verifyToken);
 
 backend.use(cors());
 backend.use(bodyParser());
@@ -28,7 +30,7 @@ frontend.use(mount("/", serve("../lutris-imaginarium/dist")));
 
 // start the server
 backend.listen(3003, "127.0.0.1");
-console.log("Backend Listening at port 3001, 127.0.0.1");
+console.log("Backend Listening at port 3003, 127.0.0.1");
 
-frontend.listen(3001, "127.0.0.1");
-console.log("Frontend listening at port 3003, 127.0.0.1");
+// frontend.listen(3001, "127.0.0.1");
+console.log("Frontend listening at port 3001, 127.0.0.1");
